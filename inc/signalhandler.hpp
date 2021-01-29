b3859ff4c08425751074fc128414054664d25ac4
MH2033 <mh.akbari@hotmail.com>
2021-01-29 17:14:44 +0330

    Add signal handler for SIGINT and SIGTSTP

---
+++ inc/signalhandler.hpp
@@ -0,0 +1,6 @@
+#ifndef SIGNAL_HANDLER_H
+#define SIGNAL_HANDLER_H
+
+void signalHandler(int);
+
+#endif
---
+++ src/signalhandler.cpp
@@ -0,0 +1,14 @@
+#include <csignal>
+#include <iostream>
+#include <climits>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+void signalHandler(int sigNum){
+    // cin.ignore(std::numeric_limits<std::streamsize>::max());
+    // cin.clear();
+    // std::cin.ignore(INT_MAX);
+    // fflush(stdin);
+    // getchar();
+}
